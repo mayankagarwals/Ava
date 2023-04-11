eecbe974929759abac56d30023efda629a5c9c25
Mayank Agarwal <mayankagarwal44442@gmail.com>
2023-04-11 19:03:38 +0530

    wrap widget in expanded

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -16,20 +16,22 @@ class MyApp extends StatefulWidget{
 }
 
 Widget _buildMessageContainer(List<String> messages) {
-  return Container(
-    margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-    child: Column(
-      children: messages.map((message) {
-        return Container(
-          margin: EdgeInsets.only(bottom: 8.0),
-          padding: EdgeInsets.all(16.0),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(8.0),
-          ),
-          child: Text(message),
-        );
-      }).toList(),
+  return Expanded(
+    child: Container(
+      margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+      child: Column(
+        children: messages.map((message) {
+          return Container(
+            margin: EdgeInsets.only(bottom: 8.0),
+            padding: EdgeInsets.all(16.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            child: Text(message),
+          );
+        }).toList(),
+      ),
     ),
   );
 }
@@ -52,43 +54,18 @@ class _MyAppState extends State<MyApp> {
 
       home: Scaffold(
         backgroundColor: Color(0xff292F3F),
+        appBar: AppBar(title: Text("hello world")),
         body: Column(
           children: [
-            Container(
-
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                  formattedDate,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 24.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                )]
-              ),
-              constraints: BoxConstraints(
-                minWidth: 0.0,
-                maxWidth: double.infinity,
-                minHeight: 0.0,
-                maxHeight: double.infinity,
-              ),
-            ),
 
             _buildMessageContainer(myList),
 
-            Positioned(
-              bottom: 16.0, //how to make this device independent
-              left: 16.0,
-              right: 16.0,
-              child: Padding(
-                padding: EdgeInsets.all(16.0),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Write',
-                    border: OutlineInputBorder(),
-                  ),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Write',
+                  border: OutlineInputBorder(),
                 ),
               ),
             ),
